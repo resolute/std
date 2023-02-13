52b25a119bc50ceea1de708c7074a2111403f773
Adam Chalemian <adam.chalemian@resolute.com>
2023-02-13 17:43:23 -0500

    deps: update deno config and tooling

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 rm -rf coverage/
-deno test --jobs 8 --import-map=import_map.json --allow-net --doc --coverage=coverage/tmp --unstable
+deno test --parallel --allow-net --doc --coverage=coverage/tmp --unstable
 deno coverage coverage/tmp --lcov > coverage/lcov.info
 sed -i.bak -E 's/SF:.*\/([^/]+\.ts)/SF:\1/' coverage/lcov.info
 rm -f coverage/lcov.info.bak
